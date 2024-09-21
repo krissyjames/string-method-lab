c96f10a16bcf84abfd5413be433076bb95f9a81c
krissyjames <kristina.ashley.cook@gmail.com>
2024-09-20 21:39:57 -0400

    completion of doesInclude

--- src/stringMethods.js
+++ src/stringMethods.js
@@ -19,10 +19,26 @@ function concatenateStrings(string1, string2) {
     }   
 }
 
+function doesInclude(string1, string2) {
+    //let string1 = string1.includes("hello world");
+    //let string2 = string.includes("world");
+    let text = "hello world"
+    let result1 = text.includes(string1)
+    let result2 = text.includes(string2)
+    if ((result1 === true) && (result2 === true) && (string1 !== "") && (string2 !== "")) {
+        return true;
+    } else if (typeof string1 !== 'string') {
+        return `Value given to function was of Data Type: ${typeof string1}`;
+    } else if (typeof string2 !== 'string') {
+        return `Value given to function was of Data Type: ${typeof string2}`;
+    } else if ((string1 === "") || (string2 == "")) {
+        return "String is empty"
+    }
+}
 module.exports = {
     getCharAt,
     concatenateStrings,
-//     doesInclude,
+    doesInclude,
 //     getIndexOf,
 //     sliceString,
 //     splitString,
